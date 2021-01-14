4b318675bec80ad6318a5fddb6963e32cb660b29
Vladimir <mrvladimiranurov@gmail.com>
2021-01-14 15:01:50 +0300

    desktop ready

--- docs/js/main.js
+++ docs/js/main.js
@@ -27,6 +27,18 @@ overlay.addEventListener('click',function(){
     mobMenu.classList.remove('active');
     bodyEl.classList.remove('noscroll')
 });
+// Подключение точек пагинации справа page-nav
+$('#page-nav').onePageNav({
+    currentClass: 'active',
+    changeHash: false,
+    scrollSpeed: 750,
+    scrollThreshold: 0.5,
+    filter: '',
+    easing: 'swing',
+    begin: function () {},
+    end: function () {},
+    scrollChange: function ($currentListItem) {}
+});
 
 
 
